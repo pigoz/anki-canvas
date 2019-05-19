a02167da056050b1e3f84a2654fe7c4c9eea1ad9
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2019-05-19 11:51:46 +0200

    front: delay execution to second frame
    This allows the Night Mode plugin to setup it's night_mode class that
    we use to decide which colorscheme to use to render our UI.

--- src/front.ts
+++ src/front.ts
@@ -17,78 +17,82 @@ import { constant } from './brushcolor';
 import * as styles from './styles';
 import * as icons from './icons';
 
-const h = hs.context();
+function init() {
+  const h = hs.context();
 
-const colorScheme = options.colorScheme();
+  const colorScheme = options.colorScheme();
 
-const canvas = h('canvas', {
-  style: styles.canvas(colorScheme),
-  width: options.frontCanvasSize * options.hdpiFactor,
-  height: options.frontCanvasSize * options.hdpiFactor,
-});
+  const canvas = h('canvas', {
+    style: styles.canvas(colorScheme),
+    width: options.frontCanvasSize * options.hdpiFactor,
+    height: options.frontCanvasSize * options.hdpiFactor,
+  });
 
-const buttons = {
-  undo: h('button', { style: styles.action(colorScheme) }),
-  clear: h('button', { style: styles.action(colorScheme) }),
-};
-
-const actions = h(
-  'div',
-  { style: styles.actions(colorScheme) },
-  Object.values(buttons),
-);
-
-const T = h('div', { style: styles.wrapper(colorScheme) }, [canvas, actions]);
-
-renderdom('ac-front', T);
-
-const state = empty();
-
-const handler = (canvas: HTMLCanvasElement, state: State, action: Action) => (
-  evt: Event,
-): void => {
-  evt.preventDefault();
-
-  if (!(evt instanceof TouchEvent) && !(evt instanceof MouseEvent)) {
-    return;
-  }
-
-  const touch = evt instanceof TouchEvent ? evt.changedTouches[0] : evt;
-
-  const point: Point = {
-    x: (touch.pageX - canvas.offsetLeft) * options.hdpiFactor,
-    y: (touch.pageY - canvas.offsetTop) * options.hdpiFactor,
+  const buttons = {
+    undo: h('button', { style: styles.action(colorScheme) }),
+    clear: h('button', { style: styles.action(colorScheme) }),
   };
 
-  action(state, point);
-};
+  const actions = h(
+    'div',
+    { style: styles.actions(colorScheme) },
+    Object.values(buttons),
+  );
 
-const events: Array<[string, Action]> = [
-  ['touchstart', addFirstDrawingPoint],
-  ['touchmove', addDrawingPoint],
-  ['touchend', addLastDrawingPoing],
-  ['mousedown', addFirstDrawingPoint],
-  ['mousemove', addDrawingPoint],
-  ['mouseup', addLastDrawingPoing],
-];
+  const T = h('div', { style: styles.wrapper(colorScheme) }, [canvas, actions]);
 
-events.forEach(e => {
-  canvas.addEventListener(e[0], handler(canvas, state, e[1]), false);
-});
+  renderdom('ac-front', T);
 
-function renderloop() {
-  rendercanvas(canvas, state, {
-    colorizer: constant(colorScheme),
-    lineWidth: options.frontLineWidth * options.hdpiFactor,
-    colorScheme,
+  const state = empty();
+
+  const handler = (canvas: HTMLCanvasElement, state: State, action: Action) => (
+    evt: Event,
+  ): void => {
+    evt.preventDefault();
+
+    if (!(evt instanceof TouchEvent) && !(evt instanceof MouseEvent)) {
+      return;
+    }
+
+    const touch = evt instanceof TouchEvent ? evt.changedTouches[0] : evt;
+
+    const point: Point = {
+      x: (touch.pageX - canvas.offsetLeft) * options.hdpiFactor,
+      y: (touch.pageY - canvas.offsetTop) * options.hdpiFactor,
+    };
+
+    action(state, point);
+  };
+
+  const events: Array<[string, Action]> = [
+    ['touchstart', addFirstDrawingPoint],
+    ['touchmove', addDrawingPoint],
+    ['touchend', addLastDrawingPoing],
+    ['mousedown', addFirstDrawingPoint],
+    ['mousemove', addDrawingPoint],
+    ['mouseup', addLastDrawingPoing],
+  ];
+
+  events.forEach(e => {
+    canvas.addEventListener(e[0], handler(canvas, state, e[1]), false);
   });
-  requestAnimationFrame(renderloop);
+
+  function renderloop() {
+    rendercanvas(canvas, state, {
+      colorizer: constant(colorScheme),
+      lineWidth: options.frontLineWidth * options.hdpiFactor,
+      colorScheme,
+    });
+    requestAnimationFrame(renderloop);
+  }
+
+  renderloop();
+
+  canvas.addEventListener('click', e => e.preventDefault(), false);
+  buttons.undo.addEventListener('click', () => undo(state), false);
+  buttons.clear.addEventListener('click', () => clear(state), false);
+  buttons.undo.innerHTML = icons.undo;
+  buttons.clear.innerHTML = icons.clear;
 }
 
-renderloop();
-
-canvas.addEventListener('click', e => e.preventDefault(), false);
-buttons.undo.addEventListener('click', () => undo(state), false);
-buttons.clear.addEventListener('click', () => clear(state), false);
-buttons.undo.innerHTML = icons.undo;
-buttons.clear.innerHTML = icons.clear;
+requestAnimationFrame(init);
